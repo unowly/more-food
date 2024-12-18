80404881eacb627cea5184b08e141f93b759fc91
robinstahl <mail@stahlrobin.de>
2024-12-18 21:16:59 +0100

    initial setup

---
+++ src/client/java/dev/unowly/MoreFoodClient.java
@@ -0,0 +1,11 @@
+package dev.unowly;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreFoodClient implements ClientModInitializer {
+	@Override
+	public void onInitia
+	lizeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
